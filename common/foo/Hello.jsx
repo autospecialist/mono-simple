e23c12b23c85ea2ce482d80668aa5d93fda1d4eb
nail <otvet99@gmail.com>
2019-08-08 17:06:12 +0200

    foo upd

--- common/foo/Hello.jsx
+++ common/foo/Hello.jsx
@@ -3,9 +3,9 @@ import React from "react";
 // Dummy component to play with monorepo
 
 function Heading({ level = "1", title }) {
-  let prefix = '"'
+  let prefix = ' "'
   let postfix = '"'
-  return React.createElement(`h${level}`, {}, level>1 ? prefix+title+postfix : title);
+  return React.createElement(`h${level}`, {}, level > 1 ? prefix + title + postfix : title);
 }
 
 export default Heading;
